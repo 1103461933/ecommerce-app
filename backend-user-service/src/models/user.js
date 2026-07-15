aa642fe0bab1fa24cfa837ecfaf08c18ef9ccb13
Xavier Encarnacion <xencarnacion91@gmail.com>
2026-07-14 23:26:27 -0500

    addin upercase for user

--- backend-user-service/src/models/user.js
+++ backend-user-service/src/models/user.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true }, 
   name: { type: String, required: true },
   role: { type: String, enum: ['user', 'admin'], default: 'user' },
 }, { timestamps: true });
